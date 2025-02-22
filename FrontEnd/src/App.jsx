1f5af8c4805d6332f523220811a78c803329913e
Nemurs <kevinoctaviomejia@gmail.com>
2025-02-21 21:38:46 -0800

    added basic FrontEnd Structure using Vite

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,21 @@
+import hooperAnalyticsLogo from '/FirePixelBall.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://github.com/Nemurs/HooperAnalytics" target="_blank">
+          <img src={hooperAnalyticsLogo} className="logo" alt="Hooper Analytics logo" />
+        </a>
+      </div>
+      <h1>Hooper Analytics</h1>
+      <p className="read-the-docs">
+        🚧 Site Under Construction 🚧
+      </p>
+    </>
+  )
+}
+
+export default App
